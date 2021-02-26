d8bb634a5cee1e7d81e4180e01a6beb5a547f898
Wen Liu <wenliu1025@gmail.com>
2021-02-26 14:54:14 +0800

    update libs

--- app/src/main/java/com/wenliu/kotlin/MainActivity.kt
+++ app/src/main/java/com/wenliu/kotlin/MainActivity.kt
@@ -5,23 +5,48 @@ import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.wenliu.kotlin.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = javaClass.simpleName
+    private val TAG = "MainActivity"
     private lateinit var binding: ActivityMainBinding
     private lateinit var user: User
+
+    val demoFlow = flow {
+        listOf(9, 5, 2, 7).forEach {
+            delay(100)
+            emit(it)
+        }
+    }
+
+    val demoFlow2 = flow {
+        listOf("0", "2", "0", "4").forEach {
+            delay(200)
+            emit(it)
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        user = User("12", "123")
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.user = user
-        binding.btnResetUserName.setOnClickListener { resetUserName() }
+
+        runBlocking {
+            demoFlow
+                .flatMapMerge {
+                    printLog("get flow $it")
+                    demoFlow2
+                }.collect {
+                    println(it)
+                }
+        }
     }
 
-    private fun resetUserName() {
-        binding.tvUserName.text = "1111111"
-        Log.i(TAG, "user.name= " + user.name)
+    private fun printLog(log: String) {
+        Log.i(TAG, log)
     }
 }
